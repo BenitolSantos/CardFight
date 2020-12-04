0bdf744573c678050819b26aad9e72df0804b740
benitolsantos <github email address>
2020-12-04 01:53:53 -0500

    add drop table to create users migration file

--- CardFight-backend/db/migrate/20201123083210_create_users.rb
+++ CardFight-backend/db/migrate/20201123083210_create_users.rb
@@ -1,7 +1,11 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
-  def change
+  def up
     create_table :users do |t|
       t.integer :health
     end
+  end
+
+  def def down 
+    drop_table :users
   end
 end
