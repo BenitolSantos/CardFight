79a1ca9b0dc3a62125c62e664a81d350872c34f9
Benito_Santos <benitolsantos@gmail.com>
2021-02-02 20:45:26 -0500

    add new create destroy and user_params to SessionsController

--- CardFight-backend/app/controllers/sessions_controller.rb
+++ CardFight-backend/app/controllers/sessions_controller.rb
@@ -1,2 +1,16 @@
 class SessionsController < ApplicationController
+    def new
+    end
+
+    def create
+    end
+
+    def auth
+    end
+
+    def destroy
+    end
+
+    def user_params
+    end
 end
