bcfbd0fa38180bb4548fdc2b6e77454598460ee2
benitolsantos <github email address>
2021-04-28 23:53:01 -0400

    branch merge fix

--- CardFight-backend/app/controllers/user_controller.rb
+++ CardFight-backend/app/controllers/user_controller.rb
@@ -1,4 +1,25 @@
 class UserController < ApplicationController
+  def home
+  end
+
   def moves
+  end
+
+  def new 
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy 
   end
 end
